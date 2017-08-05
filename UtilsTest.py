ea47469340c2b94e27adaf7a33107f20838bcd75
igbeni <iggorbeni@gmail.com>
2017-08-05 01:21:46 -0300

    Tests have been documented.

--- UtilsTest.py
+++ UtilsTest.py
@@ -7,10 +7,16 @@ test_images_folder = "test_images/"
 
 
 class UtilsTest(unittest.TestCase):
-    def image_urls_length(self):
+    def image_urls_amount(self):
+        """
+        Test for the amount of urls read from the json file.
+        """
         self.assertEqual(len(get_images_urls()), 10)
 
     def resize_image_small(self):
+        """
+        Testing image resizing to small size.
+        """
         for image in os.listdir(test_images_folder):
             file = open(test_images_folder + image, 'rb')
             resized_image = resize_image(file.read(), Utils.small_dim)
@@ -19,6 +25,9 @@ class UtilsTest(unittest.TestCase):
             self.assertEqual((width, height), Utils.small_dim)
 
     def resize_image_medium(self):
+        """
+        Testing image resizing to medium size.
+        """
         for image in os.listdir(test_images_folder):
             file = open(test_images_folder + image, 'rb')
             resized_image = resize_image(file.read(), Utils.medium_dim)
@@ -27,6 +36,9 @@ class UtilsTest(unittest.TestCase):
             self.assertEqual((width, height), Utils.medium_dim)
 
     def resize_image_large(self):
+        """
+        Testing image resizing to large size.
+        """
         for image in os.listdir(test_images_folder):
             file = open(test_images_folder + image, 'rb')
             resized_image = resize_image(file.read(), Utils.large_dim)
@@ -35,6 +47,9 @@ class UtilsTest(unittest.TestCase):
             self.assertEqual((width, height), Utils.large_dim)
 
     def generate_resized_images(self):
+        """
+        Testing the creation of resized images.
+        """
         content = open(test_images_folder + "image0.jpg", 'rb').read()
         resized_images = {'small': resize_image(content, Utils.small_dim),
                           'medium': resize_image(content, Utils.medium_dim),
